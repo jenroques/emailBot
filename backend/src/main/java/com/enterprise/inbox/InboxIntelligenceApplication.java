05ea9cb3dd3f825e8d23d681cd54e60ca9742b6c
Jennifer Roques <jenniferroques@gmail.com>
2026-02-15 13:47:44 -0600

    Setup up main App and Health Java

---
+++ backend/src/main/java/com/enterprise/inbox/InboxIntelligenceApplication.java
@@ -0,0 +1,12 @@
+package com.enterprise.inbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxIntelligenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InboxIntelligenceApplication.class, args);
+    }
+}
