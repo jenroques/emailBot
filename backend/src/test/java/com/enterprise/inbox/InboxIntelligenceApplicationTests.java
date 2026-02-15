613b5bebd95b61996b8c7279d27954f696e3bd43
Jennifer Roques <jenniferroques@gmail.com>
2026-02-15 13:47:52 -0600

    tests

---
+++ backend/src/test/java/com/enterprise/inbox/InboxIntelligenceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.enterprise.inbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxIntelligenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
